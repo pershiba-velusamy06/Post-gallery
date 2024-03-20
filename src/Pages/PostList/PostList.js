eb9c7741029f123447048543687724f26d237cca
pershiba-velusamy06 <pershiba@elred.io>
2024-03-20 12:17:10 +0530

    css for Post list

--- src/Pages/PostList/PostList.js
+++ src/Pages/PostList/PostList.js
@@ -1,20 +1,22 @@
 import React from 'react';
-
-
-const PostList = ({Post}) => {
-   
-
+import './Post.css'
+const PostList = ({ Post }) => {
     return (
         <div className="container">
-            <div className="row">
+            <div className="row row-cols-1 row-cols-md-3">
                 {Post.map((postDetails, index) => (
-                    <div key={index} className="col-md-4 mb-4">
-                        <div className="card">
-                            <div className="card-body">
+                    <div key={index} className="col mb-4 d-flex align-items-stretch">
+                        <div className="card w-100">
+                            <div className="card-body d-flex flex-column">
                                 <h5 className="card-title">{postDetails.title}</h5>
                                 <h6 className="card-subtitle mb-2 text-muted">User ID: {postDetails.userId}</h6>
-                                <p className="card-text">{postDetails.body}</p>
+                                <p className="card-text flex-grow-1">{postDetails.body}</p>
                                 <p className="card-text">Post ID: {postDetails?.id}</p>
+                                <div className="mt-auto button-display">
+                                <button className= " bg-white  btn-delete-css">delete</button>
+                                    <button className="bg-white  btn-edit-css" >Edit</button>
+                               
+                                </div>
                             </div>
                         </div>
                     </div>
